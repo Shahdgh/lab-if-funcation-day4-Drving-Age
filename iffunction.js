e7d4c8e66f2ad9d88962da6f480acebdbaa66701
Shahdgh <Shahad.Mohammed.abt.js@tuwaiq.edu.sa>
2021-10-13 10:34:07 +0300

    lab day 4

---
+++ iffunction.js
@@ -0,0 +1,9 @@
+let age = 14
+let allwadage = 18 - age
+    if (age < 18 ){
+      console.log("Sorry, you can't drive yet" )
+      console.log( "Sorry, you have " + allwadage + " years to wait until you can drive")
+    }if ( age >= 18) {
+      console.log ("You can drive, nice car by the way"  )
+    } 
+
